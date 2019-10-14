92974a182fe1cd754aa902498cddd8c65e345b01
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2019-10-14 11:57:48 +0200

    Add validation

--- src/main/java/won/bot/randomsimulatorbot/impl/RandomSimulatorBot.java
+++ src/main/java/won/bot/randomsimulatorbot/impl/RandomSimulatorBot.java
@@ -10,11 +10,15 @@
  */
 package won.bot.randomsimulatorbot.impl;
 
+import java.lang.invoke.MethodHandles;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import won.bot.framework.bot.base.EventBot;
 import won.bot.framework.eventbot.EventListenerContext;
 import won.bot.framework.eventbot.action.BaseEventBotAction;
+import won.bot.framework.eventbot.action.impl.LogAction;
 import won.bot.framework.eventbot.action.impl.MultipleActions;
 import won.bot.framework.eventbot.action.impl.ProbabilisticSelectionAction;
 import won.bot.framework.eventbot.action.impl.RandomDelayedAction;
@@ -26,8 +30,11 @@ import won.bot.framework.eventbot.action.impl.counter.IncrementCounterAction;
 import won.bot.framework.eventbot.action.impl.lifecycle.SignalWorkDoneAction;
 import won.bot.framework.eventbot.action.impl.wonmessage.CloseConnectionAction;
 import won.bot.framework.eventbot.action.impl.wonmessage.OpenConnectionAction;
+import won.bot.framework.eventbot.action.impl.wonmessage.PrintWonMessageAction;
 import won.bot.framework.eventbot.action.impl.wonmessage.SendFeedbackForHintAction;
 import won.bot.framework.eventbot.action.impl.wonmessage.SendMessageAction;
+import won.bot.framework.eventbot.behaviour.BotBehaviour;
+import won.bot.framework.eventbot.behaviour.ExecuteWonMessageCommandBehaviour;
 import won.bot.framework.eventbot.bus.EventBus;
 import won.bot.framework.eventbot.event.AtomCreationFailedEvent;
 import won.bot.framework.eventbot.event.Event;
@@ -38,25 +45,27 @@ import won.bot.framework.eventbot.event.impl.wonmessage.AtomHintFromMatcherEvent
 import won.bot.framework.eventbot.event.impl.wonmessage.ConnectFromOtherAtomEvent;
 import won.bot.framework.eventbot.event.impl.wonmessage.MessageFromOtherAtomEvent;
 import won.bot.framework.eventbot.event.impl.wonmessage.OpenFromOtherAtomEvent;
+import won.bot.framework.eventbot.event.impl.wonmessage.WonMessageReceivedOnConnectionEvent;
+import won.bot.framework.eventbot.event.impl.wonmessage.WonMessageSentOnConnectionEvent;
 import won.bot.framework.eventbot.listener.BaseEventListener;
 import won.bot.framework.eventbot.listener.EventListener;
 import won.bot.framework.eventbot.listener.impl.ActionOnEventListener;
 import won.bot.framework.eventbot.listener.impl.ActionOnceAfterNEventsListener;
-
-import java.lang.invoke.MethodHandles;
+import won.bot.randomsimulatorbot.impl.action.ValidateConnectionAction;
 
 /**
  *
  */
 public class RandomSimulatorBot extends EventBot {
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    private static final double PROB_OPEN_ON_HINT = 0.3;
-    private static final double PROB_MESSAGE_ON_OPEN = 0.5;
-    private static final double PROB_MESSAGE_ON_MESSAGE = 0.5;
-    private static final long MIN_RECATION_TIMEOUT_MILLIS = 1 * 1000;
-    private static final long MAX_REACTION_TIMEOUT_MILLIS = 3 * 1000;
-    private static final long MIN_NEXT_CREATION_TIMEOUT_MILLIS = 1 * 1000;
-    private static final long MAX_NEXT_CREATION_TIMEOUT_MILLIS = 3 * 1000;
+    private static final double PROB_OPEN_ON_HINT = 0.5;
+    private static final double PROB_MESSAGE_ON_OPEN = 0.7;
+    private static final double PROB_MESSAGE_ON_MESSAGE = 0.7;
+    private static final double PROB_VALIDATE_ON_MESSAGE = 0.5;
+    private static final long MIN_RECATION_TIMEOUT_MILLIS = 5 * 1000;
+    private static final long MAX_REACTION_TIMEOUT_MILLIS = 15 * 1000;
+    private static final long MIN_NEXT_CREATION_TIMEOUT_MILLIS = 10 * 1000;
+    private static final long MAX_NEXT_CREATION_TIMEOUT_MILLIS = 15 * 1000;
     protected BaseEventListener groupMemberCreator;
     protected BaseEventListener workDoneSignaller;
 
@@ -114,6 +123,11 @@ public class RandomSimulatorBot extends EventBot {
         bus.subscribe(AtomCreatedEvent.class, new ActionOnEventListener(ctx, new RandomDelayedAction(ctx,
                         MIN_NEXT_CREATION_TIMEOUT_MILLIS, MAX_NEXT_CREATION_TIMEOUT_MILLIS, this.hashCode(),
                         new CreateAtomWithSocketsAction(ctx, getBotContextWrapper().getAtomCreateListName()))));
+        // print to console when we receive a message
+        EventListener messagePrinter = new ActionOnEventListener(ctx, "message-printer",
+                        new PrintWonMessageAction(ctx));
+        bus.subscribe(WonMessageReceivedOnConnectionEvent.class, messagePrinter);
+        bus.subscribe(WonMessageSentOnConnectionEvent.class, messagePrinter);
         // when a hint is received, connect fraction of the cases after a random timeout
         bus.subscribe(AtomHintFromMatcherEvent.class, new ActionOnEventListener(ctx, "hint-reactor",
                         new RandomDelayedAction(ctx, MIN_RECATION_TIMEOUT_MILLIS, MAX_REACTION_TIMEOUT_MILLIS,
@@ -131,6 +145,8 @@ public class RandomSimulatorBot extends EventBot {
                                         new ProbabilisticSelectionAction(ctx, PROB_MESSAGE_ON_OPEN,
                                                         (long) this.hashCode(), new OpenConnectionAction(ctx, "Hi!"),
                                                         new CloseConnectionAction(ctx, "Bye!"))));
+        BotBehaviour behaviour = new ExecuteWonMessageCommandBehaviour(ctx);
+        behaviour.activate();
         bus.subscribe(OpenFromOtherAtomEvent.class, opener);
         bus.subscribe(ConnectFromOtherAtomEvent.class, opener);
         // when an open is received, send message or close randomly after a random
@@ -139,12 +155,32 @@ public class RandomSimulatorBot extends EventBot {
                         new RandomDelayedAction(ctx, MIN_RECATION_TIMEOUT_MILLIS, MAX_REACTION_TIMEOUT_MILLIS,
                                         (long) this.hashCode(),
                                         new ProbabilisticSelectionAction(ctx, PROB_MESSAGE_ON_MESSAGE,
-                                                        (long) this.hashCode(), new SendMessageAction(ctx, "Test message"),
+                                                        (long) this.hashCode(),
+                                                        new SendMessageAction(ctx, "Test message"),
                                                         new CloseConnectionAction(ctx, "Bye!"))));
+        EventListener validator = new ActionOnEventListener(ctx, "connection-validator",
+                        new RandomDelayedAction(ctx, MIN_RECATION_TIMEOUT_MILLIS, MAX_REACTION_TIMEOUT_MILLIS,
+                                        (long) this.hashCode(),
+                                        new ProbabilisticSelectionAction(ctx, PROB_VALIDATE_ON_MESSAGE,
+                                                        (long) this.hashCode(),
+                                                        new ValidateConnectionAction(ctx),
+                                                        new LogAction(ctx,
+                                                                        "Not validating the connection at this time"))));
         bus.subscribe(MessageFromOtherAtomEvent.class, replyer);
         bus.subscribe(OpenFromOtherAtomEvent.class, replyer);
+        // we close a connection, validate it:
+        bus.subscribe(MessageFromOtherAtomEvent.class, validator);
         // When the atomproducer is exhausted, stop.
         this.workDoneSignaller = new ActionOnEventListener(ctx, "workDoneSignaller", new SignalWorkDoneAction(ctx), 1);
         bus.subscribe(AtomProducerExhaustedEvent.class, this.workDoneSignaller);
+        System.out.println("Bot Configuration:");
+        System.out.println("------------------");
+        System.out.println("Probability of reacting to a Hint with Open         : " + PROB_OPEN_ON_HINT);
+        System.out.println("Probability of reacting to an Open with a Message   : " + PROB_MESSAGE_ON_OPEN);
+        System.out.println("Probability of reacting to a Message with a Message : " + PROB_MESSAGE_ON_MESSAGE);
+        System.out.println("Creating atoms after waiting from " + MIN_NEXT_CREATION_TIMEOUT_MILLIS + " to "
+                        + MAX_NEXT_CREATION_TIMEOUT_MILLIS + " milliseconds");
+        System.out.println("Reacting to messages after waiting from " + MIN_RECATION_TIMEOUT_MILLIS + " to "
+                        + MAX_REACTION_TIMEOUT_MILLIS + " milliseconds");
     }
 }
